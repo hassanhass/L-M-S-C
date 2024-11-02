e5e946a9d0d228f6bad1c45226b7cce35aacbc1c
hassan_000 <salem.cf11@gmail.com>
2024-11-02 14:33:41 +0300

    last one i hope

--- server/api/attendance/check-in.post.ts
+++ server/api/attendance/check-in.post.ts
@@ -2,13 +2,12 @@ import { eq, desc } from 'drizzle-orm';
 import { attendance } from '~/server/database/schema';
 
 export default defineEventHandler(async (event) => {
-    const { employee_id } = await readBody(event);
+    const user = await useMe(event, 'employee');
     const currentTime = new Date().toISOString();
-
     const lastAttendanceRecord = await useDrizzle()
         .select()
         .from(attendance)
-        .where(eq(attendance.employee_id, employee_id))
+        .where(eq(attendance.employee_id, user.employee?.id!))
         .orderBy(desc(attendance.check_in_time))
         .limit(1)
         .get();
@@ -18,13 +17,13 @@ export default defineEventHandler(async (event) => {
             const newAttendanceRecord = await useDrizzle()
                 .insert(attendance)
                 .values({
-                    employee_id,
+                    employee_id: user.employee?.id!,
                     check_in_time: currentTime,
                     check_out_time: null,
                 })
                 .execute();
 
-            return { message: "Check-in successful", newAttendanceRecord };
+            return { message: "Check-in successful" };
         } else {
             const updatedRecord = await useDrizzle()
                 .update(attendance)
@@ -34,18 +33,18 @@ export default defineEventHandler(async (event) => {
                 .where(eq(attendance.id, lastAttendanceRecord.id))
                 .execute();
 
-            return { message: "Check-out successful", updatedRecord };
+            return { message: "Check-out successful"};
         }
     } else {
         const newAttendanceRecord = await useDrizzle()
             .insert(attendance)
             .values({
-                employee_id,
+                employee_id: user.employee?.id!,
                 check_in_time: currentTime,
                 check_out_time: null,
             })
             .execute();
 
-        return { message: "Check-in successful", newAttendanceRecord };
+        return { message: "Check-in successful" };
     }
 });
